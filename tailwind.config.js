6152422857ddcefb0ffe966d652508d4dafcd891
zeethefedev <zeethefrontenddev@gmail.com>
2024-06-26 23:52:39 +0700

    add custom font

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    fontFamily: {
+      Barlow: ["Barlow Semi Condensed", "sans-serif"],
+    },
     extend: {},
   },
   plugins: [],
