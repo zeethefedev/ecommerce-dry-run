2c4b337b69e6644e15707eb4ad1a47c70c27e8c4
zeethefedev <zeethefrontenddev@gmail.com>
2024-08-06 16:55:51 +0700

    remove unused

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/Home";
-import About from "./pages/About";
 import Products from "./pages/Products";
 import MyCart from "./pages/MyCart";
 import Navbar from "./components/Navbar";
@@ -13,7 +12,6 @@ function App() {
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route path="home" element={<Home />} />
-        <Route path="about" element={<About />} />
         <Route path="products" element={<Products />} />
         <Route path="cart" element={<MyCart />} />
       </Routes>
