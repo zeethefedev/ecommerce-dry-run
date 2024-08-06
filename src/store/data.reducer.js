12da4f85c92491243c5cedcd94ad66a2d920bd11
zeethefedev <zeethefrontenddev@gmail.com>
2024-08-06 22:58:43 +0700

    data reducer for api (implemented later)

---
+++ src/store/data.reducer.js
@@ -0,0 +1,11 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const dataSlice = createSlice({
+  name: "cart",
+  initialState: {},
+  reducers: {},
+});
+
+export const {} = dataSlice.actions;
+
+export default dataSlice.reducer;
--- src/store/store.js
+++ src/store/store.js
@@ -1,6 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 import cartReducer from "./cart.reducer";
+import dataReducer from "./data.reducer";
 
 export default configureStore({
-  reducer: { cart: cartReducer },
+  reducer: { cart: cartReducer, data: dataReducer },
 });
